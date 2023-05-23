5829ae3e264634d29fcf68d28e099f0e33238c80
Mariano Camarzana <mcamarzana@technisys.com>
2023-05-23 11:25:54 -0300

    Update uri in index file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000", // change to YOUR own production server
+  uri: "catstronauts-server-production-d60f.up.railway.app", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
